41955d5113bfffbab2d35deea95a3cffa8732363
KissSmile <zhengjianliang2008@qq.com>
2020-10-27 16:05:59 +0800

    第一次提交

---
+++ MCToolModule/MCToolModule.h
@@ -0,0 +1,18 @@
+//
+//  MCToolModule.h
+//  MCToolModule
+//
+//  Created by zhengjianliang on 2020/10/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MCToolModule.
+FOUNDATION_EXPORT double MCToolModuleVersionNumber;
+
+//! Project version string for MCToolModule.
+FOUNDATION_EXPORT const unsigned char MCToolModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCToolModule/PublicHeader.h>
+
+
